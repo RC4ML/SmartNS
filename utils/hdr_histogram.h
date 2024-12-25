7d5d242bb2f58951f403d4433ee00acbde7b60e9
cxz66666 <1092889706@qq.com>
2024-12-25 15:26:24 +0800

    chore: rename hdr_historgram utils

--- utils/hdr_histogram.h
+++ utils/hdr_histogram.h
@@ -1,29 +1,27 @@
 #include <hdr/hdr_histogram.h>
 
-namespace SmartNS {
-    class Histogram {
-    public:
-        explicit Histogram(int64_t lowest, int64_t highest, int sigfigs, double scale = 1);
+class Histogram {
+public:
+    explicit Histogram(int64_t lowest, int64_t highest, int sigfigs, double scale = 1);
 
-        explicit Histogram(int64_t lowest, int64_t highest, double scale = 1);
+    explicit Histogram(int64_t lowest, int64_t highest, double scale = 1);
 
-        ~Histogram();
+    ~Histogram();
 
-        void reset();
+    void reset();
 
-        void record(int64_t value, int64_t count = 1);
+    void record(int64_t value, int64_t count = 1);
 
-        void record_atomic(int64_t value, int64_t count = 1);
+    void record_atomic(int64_t value, int64_t count = 1);
 
-        double get_mean();
+    double get_mean();
 
-        double get_value_at_percentile(double percentile);
+    double get_value_at_percentile(double percentile);
 
-        void print(FILE *stream, int32_t ticks);
+    void print(FILE *stream, int32_t ticks);
 
-        void print_csv(FILE *stream, int32_t ticks);
-    private:
-        hdr_histogram *latency_hist;
-        double scale_value{ 1 };
-    };
-}
+    void print_csv(FILE *stream, int32_t ticks);
+private:
+    hdr_histogram *latency_hist;
+    double scale_value{ 1 };
+};
